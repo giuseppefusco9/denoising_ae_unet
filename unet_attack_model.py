3361d49c7c673815b10ee312ed8a3d9a017652cf
Giuseppe Fusco <giuseppefusco@MacBook-Air-di-Giuseppe.local>
2026-06-11 15:31:57 +0200

    modified unet model

--- unet_attack_model.py
+++ unet_attack_model.py
@@ -39,29 +39,33 @@ class UNetDenoiseAttack(nn.Module):
         self.down2 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(32,  64))
         self.down3 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(64,  128))
         self.down4 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(128, 256))
+        self.down5 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(256, 512))
 
         # --- BOTTLENECK ---
-        self.bottleneck = nn.Sequential(nn.MaxPool2d(2), DoubleConv(256, 512))
+        self.bottleneck = nn.Sequential(nn.MaxPool2d(2), DoubleConv(512, 1024))
 
         # --- DECODER (Upsampling con Skip Connections) ---
-        self.up1      = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.conv_up1 = DoubleConv(512, 256)
+        self.up1      = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.conv_up1 = DoubleConv(1024, 512)
 
-        self.up2      = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.conv_up2 = DoubleConv(256, 128)
+        self.up2      = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.conv_up2 = DoubleConv(512, 256)
 
-        self.up3      = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.conv_up3 = DoubleConv(128, 64)
+        self.up3      = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.conv_up3 = DoubleConv(256, 128)
 
-        self.up4      = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
-        self.conv_up4 = DoubleConv(64, 32)
+        self.up4      = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.conv_up4 = DoubleConv(128, 64)
 
-        self.up5      = nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2)
-        self.conv_up5 = DoubleConv(32, 16)
+        self.up5      = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
+        self.conv_up5 = DoubleConv(64, 32)
+
+        self.up6      = nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2)
+        self.conv_up6 = DoubleConv(32, 16) 
 
         # Convoluzione finale + attivazione registrata come modulo
         self.outc      = nn.Conv2d(16, out_channels, kernel_size=1)
-        self.activ_out = TanhNorm()   # visibile nel summary e nei checkpoint
+        self.activ_out = TanhNorm()
 
         self.detector = detector
 
@@ -72,16 +76,17 @@ class UNetDenoiseAttack(nn.Module):
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-        b  = self.bottleneck(x5)
+        x6 = self.down5(x5)
+        b  = self.bottleneck(x6)
 
-        # --- Decoder con skip connections ---
-        t1 = self.conv_up1(torch.cat([self.up1(b),  x5], dim=1))
-        t2 = self.conv_up2(torch.cat([self.up2(t1), x4], dim=1))
-        t3 = self.conv_up3(torch.cat([self.up3(t2), x3], dim=1))
-        t4 = self.conv_up4(torch.cat([self.up4(t3), x2], dim=1))
-        t5 = self.conv_up5(torch.cat([self.up5(t4), x1], dim=1))
+        t1 = self.conv_up1(torch.cat([self.up1(b),  x6], dim=1))
+        t2 = self.conv_up2(torch.cat([self.up2(t1), x5], dim=1))
+        t3 = self.conv_up3(torch.cat([self.up3(t2), x4], dim=1))
+        t4 = self.conv_up4(torch.cat([self.up4(t3), x3], dim=1))
+        t5 = self.conv_up5(torch.cat([self.up5(t4), x2], dim=1))
+        t6 = self.conv_up6(torch.cat([self.up6(t5), x1], dim=1))
 
-        reconstructed_imgs = self.activ_out(self.outc(t5))
+        reconstructed_imgs = self.activ_out(self.outc(t6))
 
         if self.detector is not None:
             with torch.no_grad():
